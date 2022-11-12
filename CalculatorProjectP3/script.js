bcb8f1db7c4982cc7ca1c33ee51264e420176d82
Marcin <marcin11137@gmail.com>
2022-11-12 20:20:16 +0100

    Working with calculator

--- CalculatorProjectP3/script.js
+++ CalculatorProjectP3/script.js
@@ -2,8 +2,10 @@ let displayValue = [];
 
 
 let numbersButtons = document.querySelectorAll(".buttonNumber");
-
-let specialOperators = ['+', '-', '*', '/'];
+let picked = '';
+let specialOperators = ["+", '-', '*', '/'];
+let numer1;
+let numer2;
 
 numbersButtons.forEach(button => {
 
@@ -23,7 +25,14 @@ numbersButtons.forEach(button => {
 function newValue(){
     let display = document.querySelector(".display");
 
-    display.textContent = displayValue;
+    
+        display.textContent = displayValue;
+        display.innerHTML += picked;
+
+
+    
+   
+  
 }
 
 
@@ -33,96 +42,83 @@ operatorButtons.forEach(button  => {
     
     button.addEventListener('click', () => {
 
-        
+        switchs(button.id);
       
-        switch (button.id) {
-            case 'plus':{
+    })
 
-                if( !specialOperators.includes(displayValue.slice(-1)) && displayValue.slice(-1) != ''){
-                    displayValue = displayValue + '+';
-                }
-               
-                break;
-            }
-            case 'minus': {
-                if( !specialOperators.includes(displayValue.slice(-1)) && displayValue.slice(-1) != ''){
-                    displayValue = displayValue + '-';
-                }
-                
-                break;
-            }
-            case 'multiply': {
-                if( !specialOperators.includes(displayValue.slice(-1)) && displayValue.slice(-1) != ''){
-                    displayValue = displayValue + '*';
-                }
-               
-                break;
-            }
-            case 'divide': {
-                if( !specialOperators.includes(displayValue.slice(-1)) && displayValue.slice(-1) != ''){
-                    displayValue = displayValue + '/';
-                }
-                
-                break;
-            }
-            case 'clear':{
-                displayValue = '';
-                break;
-            }
-            case 'result': {
-               
-                GoOverString();
-                break;
-            }
-            default:
-                break;
-        }
-        newValue();
-    }
-    )
+        
+       
+    
 
 });
 
-
-
-function GoOverString(){
-
-    let temporialNumber1 = "";
-    let whatToDo = '';
-    let temporialNumber2 = "";
-    let foundOperator = 0;
-
-    let lengthDisplayValue = displayValue.length;
-
-    
-    for (let i = 0; i < lengthDisplayValue; i++) {
-        if(displayValue.slice(+i) != (specialOperators))
-        if(!displayValue.slice(+i).includes(...specialOperators)){
-            temporialNumber1 = temporialNumber1 + displayValue.slice(+i +1);
-         
-        
-
-        }
-        else if(displayValue.slice(i).includes(...specialOperators)){
-            whatToDo = displayValue.slice(+i);
-            foundOperator++;
-            if(foundOperator == 2){
-                break;
-            }
-           
-        }
-        else if(!displayValue.slice(+i).includes(specialOperators)){
-            temporialNumber2 = temporialNumber2 + temporialNumber2;
-        }
+function AddAndSend(){
+    if(numer1 == null){
+        numer1 = parseInt(displayValue);
+        displayValue = '';
     }
 
-    console.log(temporialNumber1);
-    console.log(whatToDo);
-
-
-
+   
+    
 
 }
+function switchs(string){
+    switch (string) {
+        case 'plus':{
+
+            AddAndSend();
+                picked = '+';
+                
+           
+            break;
+        }
+        case 'minus': {
+        
+            AddAndSend();
+                picked = '-';
+            
+            
+            break;
+        }
+        case 'multiply': {
+            AddAndSend();
+                picked = '*';
+          
+            break;
+        }
+        case 'divide': {
+            AddAndSend();
+                picked = '/';
+            
+            
+            break;
+        }
+        case 'clear':{
+            displayValue = '';
+            break;
+        }
+        case 'result': {
+            numer2 = parseInt(displayValue);
+            console.log(numer1);
+            console.log(numer2);
+           Operate(picked, numer1, numer2)
+            break;
+        }
+        default:
+            break;
+    }
+    newValue();
+    
+}
+
+
+
+
+
+
+
+
+
 
 
 
@@ -196,24 +192,24 @@ function Operate(operator, num1, num2 ){
 
     switch (operator) {
         case '+':{
-            add(num1, num2);
+            displayValue = add(num1, num2);
             break;
         }
         case '-':{
-            subtract(num1, num2);
+            displayValue =  subtract(num1, num2);
             break;
         }   
         case '*':{
-            multiply(num1, num2);
+            displayValue =  multiply(num1, num2);
             break;
         }
         case '/':{
-            divide(num1, num2);
+            displayValue =  divide(num1, num2);
             break;
         }
 
         default:
-            return "Incorrent operator.";
+            console.log("Incorrent operator.")
             break;
     }
 }
